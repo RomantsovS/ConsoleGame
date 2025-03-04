83105b0f37987a5e5628bbe8e2d651982d3ed1ba
RomantsovS <romancov123@gmail.com>
2025-03-04 22:23:15 +0300

    fix linux console screen ctor

--- Code/sys/linux/linux_console_screen.cpp
+++ Code/sys/linux/linux_console_screen.cpp
@@ -7,7 +7,7 @@ idCVar text_info_max_height("text_info_max_height", "10",
                             CVAR_SYSTEM | CVAR_INIT, "");
 
 LinuxConsoleScreen::LinuxConsoleScreen(pos_type wd, pos_type ht, Pixel back) noexcept
-    : Screen(wd, ht, back) {
+    : Screen(wd, ht) {
     setBackGroundPixel(back);
 }
 
